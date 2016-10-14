a778385dc431ebf39e8bd270b979067aa6a4cd81
dennisviana <denisvcosta@gmail.com>
2016-10-14 13:41:34 -0300

    Update BDConfig.java

--- rest-sigo/src/br/com/sigo/config/BDConfig.java
+++ rest-sigo/src/br/com/sigo/config/BDConfig.java
@@ -5,16 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * Classe utilizada para fazer a conex„o com o banco de dados.
+ * Classe utilizada para fazer a conex√£o com o banco de dados.
  * 
  * @author Denis Viana
  *
  */
 public class BDConfig {
 	
-	private static String url = "jdbc:postgresql://localhost:5432/dbsigo";  
-    static String usuario = "snorlax";  
-    static String senha = "Sigoapp";
+	private static String url = "jdbc:postgresql://localhost:5432/database";  
+    static String usuario = "user"
+    static String senha = "senha"
 		
 	public static Connection getConnection() throws ClassNotFoundException, SQLException{
 		Class.forName("org.postgresql.Driver");
